687bc7504da6ee6f3aa76dd8f5896806b1adaf46
rodrigouberlandiamg <rso.android3dudi@gmail.com>
2022-05-23 16:56:48 -0300

    Commit de tipo de indices e tipo de metodo de retorno.

--- main.py
+++ main.py
@@ -10,8 +10,11 @@ git config --bool flake8.strict true -> Bloqueia subir para o GIT.
 '''
 
 
-from fila_normal import FilaNormal
-from fila_prioritaria import FilaPrioritaria
+# from fila_normal import FilaNormal
+# from fila_prioritaria import FilaPrioritaria
+from fabrica_fila import FabricaFila
+
+
 if __name__ == '__main__':
     # fila_teste = FilaNormal()
     # fila_teste.atualizafila()
@@ -20,11 +23,19 @@ if __name__ == '__main__':
     # print(fila_teste.chamacliente(5))
     # print(fila_teste.chamacliente(10))
 
-    fila_teste2 = FilaPrioritaria()
-    fila_teste2.atualiza_fila()
-    fila_teste2.atualiza_fila()
-    fila_teste2.atualiza_fila()
-    fila_teste2.atualiza_fila()
-    print(fila_teste2.chama_cliente(10))
-    print(fila_teste2.chama_cliente(2))
-    print(fila_teste2.estatistica('19/05/2022',1,'detail'))
+    # fila_teste2 = FilaPrioritaria()
+    # fila_teste2.atualiza_fila()
+    # fila_teste2.atualiza_fila()
+    # fila_teste2.atualiza_fila()
+    # fila_teste2.atualiza_fila()
+    # print(fila_teste2.chama_cliente(10))
+    # print(fila_teste2.chama_cliente(2))
+    # print(fila_teste2.estatistica('19/05/2022',1,'detail'))
+
+    teste_fabrica = FabricaFila.pega_fila('normal')
+    teste_fabrica.atualiza_fila()
+    teste_fabrica.atualiza_fila()
+    teste_fabrica.atualiza_fila()
+    print(teste_fabrica.chama_cliente(10))
+    print(teste_fabrica.chama_cliente(2))
+    # print(teste_fabrica.estatistica('19/05/2022',1,'detail'))
